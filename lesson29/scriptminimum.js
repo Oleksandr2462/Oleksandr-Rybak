0d69f558a0dc5712ec86e923531d5490ea26f8fc
OleksandrRybak <rybak.oleksandr2462@gmail.com>
2023-03-29 15:34:17 +0300

    add lesson30

---
+++ lesson29/scriptminimum.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let oleksandrRybak;
+
+let rybakOleksandr123;
+
+let $oleksandrRybak;
+
+let _oleksandrRybak;
+
+// let 1aoleksandrRybak;
+
+/*let oleksandr-Rybak;
+
+let Oleksandrrybak;*/
+
+
+
+
+
+
+
+
---
+++ lesson29/scriptnorma.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const CURRENT_YEAR = 2023;
+
+let nameUser = prompt('What is your name?');
+
+alert("Привіт" + " " + nameUser);
+
+let dateOfBirth = prompt('When you was born?');
+
+alert(CURRENT_YEAR - dateOfBirth);
+
+let lenghtSideOfSquare = prompt('What is the lenght side of square?');
+
+alert(lenghtSideOfSquare * 4);
+
+
